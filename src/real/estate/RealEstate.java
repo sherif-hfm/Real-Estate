c2d70990eb0b7d5f790990158043e38975ad0a50
Sherif <sherif_hfm@yahoo.com>
2020-11-10 20:18:22 +0300

    add database

--- src/real/estate/RealEstate.java
+++ src/real/estate/RealEstate.java
@@ -17,7 +17,7 @@ public class RealEstate {
     public static void main(String[] args) {
         // TODO code application logic here
         System.err.println("ok");
-         frmDueRents.main(null); 
+         frmDueRents.main(null);  
     }
     
 }
